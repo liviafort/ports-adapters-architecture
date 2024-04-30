47b2f7cc90bc1deb18e1c32a82edc3c864141419
liviafort <livia.fortunato@academico.ifpb.edu.br>
2024-04-30 15:03:54 -0300

    Using the full tokenCore implementation in the client adapter

--- src/main/kotlin/com/portsadapters/portsdapters/core/adapter/ClientAdapter.kt
+++ src/main/kotlin/com/portsadapters/portsdapters/core/adapter/ClientAdapter.kt
@@ -1,6 +1,7 @@
 package com.portsadapters.portsdapters.core.adapter
 
 import com.portsadapters.portsdapters.core.model.ClientCore
+import com.portsadapters.portsdapters.core.model.TokenCore
 import com.portsadapters.portsdapters.core.ports.EntrypointPort
 import com.portsadapters.portsdapters.core.ports.TokenStsPort
 import org.springframework.stereotype.Service
@@ -10,8 +11,8 @@ class ClientAdapter(
     private val tokenStsPort: TokenStsPort
 ): EntrypointPort {
     override fun getClientByCpf(cpf: String): ClientCore {
-        val acess_token = tokenStsPort.getTokenSts()
-        print(acess_token)
+        val token: TokenCore = tokenStsPort.getTokenSts()
+        print(token)
         return ClientCore(
             name = "Livia",
             lastName = "Fortunato"
